8c442a5c317eb6241ec87b6ceec77fda7c5dc452
Adi Setiawan <adisetiawan@gmail.com>
2017-10-26 10:48:13 +0800

    added handle message function

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `Pesan kamu: "${received_message.text}". kirim aku foto dong!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);
 
 }
 
@@ -15,7 +28,27 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": "EAAFPdZCzzn8gBAFE5ONDxHT6hd8sDzfVdZCuJAkp4piJ9Y8pZAmkZCBHKIjyNJDNxIm4fJG9ikn9xwGc6vWJvt75RoOM6EyoLR0bxJV8S3MKWk2u19f9hi6XARE7Jy33xClRcvyoz9BzcYSRwcCLWIbPc2KcBWfdCvJAwOzZAuAZDZD" },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 }
 
 // Adds support for GET requests to our webhook
@@ -64,7 +97,15 @@ app.post('/webhook', (req, res) => {
       // Get the sender PSID
       let sender_psid = webhookEvent.sender.id;
       console.log('Sender PSID: ' + sender_psid);
-      
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhookEvent.message) {
+        handleMessage(sender_psid, webhookEvent.message);        
+      } else if (webhookEvent.postback) {
+        handlePostback(sender_psid, webhookEvent.postback);
+      }
+
     });
 
     // Returns a '200 OK' response to all requests
